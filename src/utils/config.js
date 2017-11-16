dc46d032d0e94f4057c99e254a532c1bc00090c3
pmg1989 <972401854@qq.com>
2017-11-16 16:37:46 +0800

    fix title

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,9 @@
 
 export default {
-  name: 'dva-admin',
+  name: '牛班音乐学校',
   prefix: '',
-  footerText: 'dva-admin 版权所有 © 2016 由 pmg1989 提供支持',
+  footerText: 'newband 版权所有 © 2016 由 felixpan 提供支持',
   logoSrc: 'https://o9u2lnvze.qnssl.com/web/global/brand.png',
-  logoText: 'DVA Admin',
+  logoText: 'NEWBAND',
   needLogin: true,
 }
