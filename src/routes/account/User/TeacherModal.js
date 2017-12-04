bf3e30410e38ef1b16737ad968afac313bce50ba
pmg1989 <972401854@qq.com>
2017-12-04 15:05:24 +0800

    fix TeacherModal.js

---
+++ src/routes/account/User/TeacherModal.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Form, Input, Modal, Icon } from 'antd'
+import { getModalType } from 'utils/dictionary'
+
+const FormItem = Form.Item
+
+const formItemLayout = {
+  labelCol: {
+    span: 4,
+  },
+  wrapperCol: {
+    span: 17,
+  },
+}
+
+const ModalForm = ({
+  modal: { curItem, type, visible },
+  loading,
+  onCancel,
+  form: {
+    getFieldDecorator,
+  },
+}) => {
+  const { name, icon } = getModalType(type)
+  const modalFormOpts = {
+    title: <div><Icon type={icon} /> {name} - 学员</div>,
+    visible,
+    maskClosable: false,
+    width: 800,
+    wrapClassName: 'vertical-center-modal',
+    confirmLoading: loading.models.accountUser,
+    onCancel,
+    footer: null,
+  }
+
+  return (
+    <Modal {...modalFormOpts}>
+      <Form>
+        <FormItem label="用户名" {...formItemLayout}>
+          {getFieldDecorator('username', {
+            initialValue: curItem.username,
+          })(<Input disabled />)}
+        </FormItem>
+      </Form>
+    </Modal>
+  )
+}
+
+ModalForm.propTypes = {
+  modal: PropTypes.object.isRequired,
+  form: PropTypes.object.isRequired,
+  loading: PropTypes.object.isRequired,
+  onCancel: PropTypes.func.isRequired,
+}
+
+export default Form.create()(ModalForm)
