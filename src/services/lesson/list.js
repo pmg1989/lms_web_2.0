c84ffcae9cc23debb3db10b8c6aab425db0613f1
pmg1989 <972401854@qq.com>
2017-12-14 10:20:57 +0800

    fix Calendar.js

--- src/services/lesson/list.js
+++ src/services/lesson/list.js
@@ -3,3 +3,7 @@ import { request } from 'utils/request'
 export async function query (params) {
   return request({ wsfunction: 'mod_frontservice_getlessons', ...params })
 }
+
+export async function deleteBatch (params) {
+  return request({ wsfunction: 'mod_frontservice_deletelessons', ...params })
+}
