48abda0812ea102cdba13589de758e7b95b54191
pmg1989 <972401854@qq.com>
2017-12-28 15:45:59 +0800

    fix lessonStudent.js

--- src/routes/lesson/ItemStudent/List.js
+++ src/routes/lesson/ItemStudent/List.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Menu, Modal } from 'antd'
+import { Menu, Modal, Radio, Row, Col } from 'antd'
 import { DataTable, DropMenu } from 'components'
 import { ADD, UPDATE, DETAIL, DELETE } from 'constants/options'
 import styles from './List.less'
@@ -32,14 +32,23 @@ function List ({
       title: '姓名',
       dataIndex: 'firstname',
       key: 'firstname',
+      width: 280,
     }, {
       title: '考勤',
       dataIndex: 'acronym',
       key: 'acronym',
+      width: 280,
+      render: acronym => (
+        <Radio.Group defaultValue={acronym}>
+          <Radio.Button value="P"><span className={styles.primary}>出席</span></Radio.Button>
+          <Radio.Button value="L"><span className={styles.warning}>迟到</span></Radio.Button>
+          <Radio.Button value="A"><span className={styles.danger}>缺席</span></Radio.Button>
+        </Radio.Group>
+      ),
     }, {
       title: '操作',
       key: 'operation',
-      // width: 80,
+      width: 120,
       render: (text, record) => (
         <DropMenu>
           <Menu onClick={({ key }) => handleMenuClick(key, record)}>
@@ -54,17 +63,46 @@ function List ({
     },
   ]
 
+  const dataTableProps = {
+    loading,
+    columns,
+    scroll: { x: 708 },
+    dataSource: list,
+    pagination: false,
+    animate: false,
+    size: 'small',
+    rowKey: record => record.id,
+  }
+
+  dataTableProps.defaultExpandAllRows = true
+  dataTableProps.expandedRowRender = (record) => {
+    const { jl_song: { song, original_singer: originalSinger, back_source: backSource } } = record
+    if (song && originalSinger) {
+      return (
+        <div className={styles.text_left}>
+          <Row>
+            <Col span={8}>歌曲名：{song}</Col>
+            <Col span={8}>原唱：{originalSinger}</Col>
+            <Col span={8}>{backSource === 2 && '不'}自带伴奏</Col>
+          </Row>
+          <Row>
+            <Col span={24}>文件名：{record.jl_recording.name}</Col>
+          </Row>
+          <Row>
+            <Col span={24}>
+              <audio controls src={record.jl_recording.url}>
+                <track kind="captions" />
+              </audio>
+            </Col>
+          </Row>
+        </div>
+      )
+    }
+    return false
+  }
+
   return (
-    <DataTable
-      className={styles.table}
-      scroll={{ x: 638 }}
-      columns={columns}
-      dataSource={list}
-      loading={loading}
-      pagination={false}
-      size="small"
-      rowKey={record => record.id}
-    />
+    <DataTable {...dataTableProps} />
   )
 }
 
