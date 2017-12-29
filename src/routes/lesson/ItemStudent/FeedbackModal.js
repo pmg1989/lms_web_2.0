9206dd616b213628809b8fd39e3f25e20a9ebcf9
pmg1989 <972401854@qq.com>
2017-12-29 13:00:33 +0800

    fix lessonStudent.js

--- src/routes/lesson/ItemStudent/FeedbackModal.js
+++ src/routes/lesson/ItemStudent/FeedbackModal.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Form, Input, Modal, Icon, Spin, Row, Col } from 'antd'
+import { Form, Input, Modal, Icon, Spin, Rate, Row, Col } from 'antd'
 import { getModalType } from 'utils/dictionary'
 
 const FormItem = Form.Item
 const { TextArea } = Input
+
+const starsText = {
+  0: '未评价',
+  1: '不满意',
+  2: '不太满意',
+  3: '满意',
+  4: '很满意',
+}
 
 const ModalForm = ({
   modal: { curItem, type, visible },
@@ -29,12 +37,47 @@ const ModalForm = ({
         <Row>
           <Col span={20} offset={2}>
             <Form layout="vertical">
-              <FormItem label="建议与意见(针对教学内容)">
+              <h3>1 老师满意度</h3><br />
+              <Row>
+                <Col offset={1}>
+                  <FormItem label="1.1 老师课前教学准备充分度">
+                    {getFieldDecorator('lesson_prepare_score', {
+                      initialValue: curItem.lesson_prepare_score,
+                    })(<Rate disabled count={4} />)}
+                    {<span>{starsText[curItem.lesson_prepare_score]}</span>}
+                  </FormItem>
+                  <FormItem label="1.2 本课内容设计满意度">
+                    {getFieldDecorator('lesson_content_score', {
+                      initialValue: curItem.lesson_content_score,
+                    })(<Rate disabled count={4} />)}
+                    {<span>{starsText[curItem.lesson_content_score]}</span>}
+                  </FormItem>
+                  <FormItem label="1.3 老师课堂形象满意度">
+                    {getFieldDecorator('teacher_appearance_score', {
+                      initialValue: curItem.teacher_appearance_score,
+                    })(<Rate disabled count={4} />)}
+                    {<span>{starsText[curItem.teacher_appearance_score]}</span>}
+                  </FormItem>
+                  <FormItem label="1.4 与老师有良好的互动">
+                    {getFieldDecorator('lesson_interaction_score', {
+                      initialValue: curItem.lesson_interaction_score,
+                    })(<Rate disabled count={4} />)}
+                    {<span>{starsText[curItem.lesson_interaction_score]}</span>}
+                  </FormItem>
+                  <FormItem label="1.5 老师讲课表达能力">
+                    {getFieldDecorator('teacher_expression_score', {
+                      initialValue: curItem.teacher_expression_score,
+                    })(<Rate disabled count={4} />)}
+                    {<span>{starsText[curItem.teacher_expression_score]}</span>}
+                  </FormItem>
+                </Col>
+              </Row>
+              <FormItem label="2 建议与意见(针对教学内容)">
                 {getFieldDecorator('lesson_suggestion', {
                   initialValue: curItem.lesson_suggestion,
                 })(<TextArea disabled rows={4} />)}
               </FormItem>
-              <FormItem label="对老师的评价(针对老师)">
+              <FormItem label="3 对老师的评价(针对老师)">
                 {getFieldDecorator('teacher_suggestion', {
                   initialValue: curItem.teacher_suggestion,
                 })(<TextArea disabled rows={4} />)}
